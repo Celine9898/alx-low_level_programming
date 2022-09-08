5d31f26b7f11a9a751ca4c662a5450f547741fad
Onyinye udechi <udechionyii@gmail.com>
2022-09-08 04:37:38 -0700

    put functions

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio .h>
+
+/**
+ * main - Entry piont
+ *
+ * Return: Always 0 (when successfully printed the function)
+ */
+int main(viod)
+{puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
