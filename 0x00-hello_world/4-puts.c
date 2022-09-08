33ac07fa059c03dc46020df325f1bcca760bf0b3
Onyinye udechi <udechionyii@gmail.com>
2022-09-08 06:22:02 -0700

    2-assembler

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio .h>
 
 /**
- * main - Entry piont
+ * main - Entry point 
  *
- * Return: Always 0 (when successfully printed the function)
+ * Return: Always 0 (when function is successful)
  */
 int main(viod)
-{puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+{
+	putchar("Programming is like building a multilingual puzzle\n")
+		return (0);
 }
