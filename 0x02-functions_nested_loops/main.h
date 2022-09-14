a6807addb74b98a86158d8487a63596766c95b15
Onyinye udechi <udechionyii@gmail.com>
2022-09-14 00:25:38 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,4 +4,11 @@
 int _putchar(char);
 void print_alphabet(void);
 
+void print_alphabet_x10(void);
+/**
+ * _islower - prints if c is lower case 
+ * @c: character to use 
+ * Return: 1 or 0
+ */
+
 #endif
