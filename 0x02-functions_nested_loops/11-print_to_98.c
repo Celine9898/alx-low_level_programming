483e0de63552b5d04f1ec5e00a82067d26f6fa8c
Onyinye udechi <udechionyii@gmail.com>
2022-09-14 12:17:05 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,39 +6,18 @@
  */
 void print_times_table(int n)
 {
-	int i;
-	int k;
 
-	if (n <= 15 && n >= 0)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			for (k = 0; k <= n; k++)
-			{
-				int prod = k * i;
-
-				if (k == 0)
-				{
-					_putchar('0');
-				} else if (prod <= 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar('0' + (prod / 100));
-					_putchar('0' + ((prod / 10) % 10));
-					_putchar('0' + (prod % 10));
-				} else
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + (prod / 10));
-					_putchar('0' + (prod % 10));
-
-				}
-			}
-			_putchar('\n');
-		}
-	}
+while (n < 98)
+{
+	printf("%i, ", n);
+	n++;
+}
+while (n > 98)
+{
+	printf("%i, ", n);
+	n--;
 }
 
+printf("98");
+putchar('\n');
+}
