39c892fa2c4df89d97057123ac5d6cd442ea3d73
Onyinye udechi <udechionyii@gmail.com>
2022-09-14 01:35:27 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,17 @@
  * Return: always 0;
  *
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	char i;
-	char h;
-	for (h = 0; h <= 9; h++)
+	char j;
+
+	for (j = 0; j <= 9; j++);
 	{
 		for (i = 'a'; i <= 'z'; i++)
 		{
 			_putchar(i);
 		}
 		_putchar('\n');
-		return (0);
+	}
 }
