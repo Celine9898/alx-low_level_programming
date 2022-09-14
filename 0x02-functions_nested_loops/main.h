1ec58236c1b9aa9e39d6ae6eadb235e195ba9967
Onyinye udechi <udechionyii@gmail.com>
2022-09-14 05:32:15 -0700

    6-abs.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -17,4 +17,12 @@ int _isalpha(int c);
  * @n: integer to input
  * Return: 1 or 0  or -1
  */
+int _abs(int);
+/**
+ * print_last_last - prints digit last
+ *
+ * @int: integer to input
+ *
+ * Return: last digit of integer
+ */
 #endif
