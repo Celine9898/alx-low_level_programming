1c403ce56aabde39b00739bdd4558379dbd9460b
Onyinye udechi <udechionyii@gmail.com>
2022-09-14 05:19:30 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,5 +12,9 @@ void print_alphabet_x10(void);
  */
 int _islower(int c);
 int _isalpha(int c);
-int print_sign(int n);
+/**
+ * print_sign - prints a sign based on n
+ * @n: integer to input
+ * Return: 1 or 0  or -1
+ */
 #endif
