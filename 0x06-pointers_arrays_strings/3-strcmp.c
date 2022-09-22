7d3f227e6b1eebe20e19ad41c6e9b0e4dac6876b
Onyinye udechi <udechionyii@gmail.com>
2022-09-21 22:45:50 -0700

    compare twso strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,16 +10,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	char *str_one = s1;
+	char *str_two = s2;
 
-	while (*s1 == *s2)
+	while (*str_one != '\0' && *str_two != '\0' && *str_one == *str_two)
 	{
-		if (*s1 == '\0')
-		{
-			return (0);
-		}
-		s1++;
-		s2++;
+		str_one++;
+		str_two++;
 	}
-	return (*s1 - *s2);
+
+return (*str_one - *str_two);
 }
