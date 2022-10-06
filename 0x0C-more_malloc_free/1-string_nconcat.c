77c5b12e49aa91effc2d08385c11fcc337dd3fb5
Onyinye udechi <udechionyii@gmail.com>
2022-10-06 15:33:35 -0700

    strinds

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,7 +9,6 @@
  *
  * Return: pointer to the resulting string
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
@@ -17,10 +16,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	while (s1 && s1[len1])
 		len1++;
-
 	while (s2 && s2[len2])
 		len2++;
-
 
 	if (n < len2)
 		s = malloc(sizeof(char) * (len1 + n + 1));
@@ -43,5 +40,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s[i++] = s2[j++];
 
 	s[i] = '\0';
+
 	return (s);
 }
